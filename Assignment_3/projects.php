46d41002c1f1509355290b073b0e36d265342dfa
Kaboudouo <chotto60@hotmail.com>
2023-04-12 17:36:36 -0400

    Finished Projects Page

--- Assignment_3/projects.php
+++ Assignment_3/projects.php
@@ -7,6 +7,22 @@
     <head>
         <title>Chayer Profile</title>
         <link rel="stylesheet" href="index.css" />
+        <script type="text/javascript">
+            function createProject(name, description){
+                const nametext = document.createTextNode(name);
+                const descriptiontext = document.createTextNode(description);
+
+                const pnode = document.createElement("p");
+                pnode.appendChild(descriptiontext);
+
+                const linode = document.createElement("li");
+                linode.appendChild(nametext);
+                linode.appendChild(pnode);
+
+                const element = document.getElementById("prj");
+                element.appendChild(linode);
+            }
+        </script>
     </head>
     <body>
         <div class="header">
@@ -23,54 +39,18 @@
         </div>
         <div class="projects">
             <h1 class="titles">PROJECTS SUMMARY</h1>
-            <ol type="I">
-                <li >
-                    Retail E-Commerce Website (WIP)
-                    <div class="box">
-                        <p>
-                            Dynamic E-Commerce website created using Javascript and React. This projects seeks to include
-                            the prevalent knowledge needed for front-end development.
-                        </p>
-                        <img src="rsc/construction.jpg" alt="construction" />
-                    </div>
-                </li>
-                    
-                <li >
-                    Thrill of the Kill - Multiplayer FPS (WIP)
-                    <div class="box">
-                        <p>
-                            Action-packed first person shooter. Created using Unity, Thrill of the Kill is a movement-heavy
-                            and ability-based game, seeking to get your adrenaline going. The purpose of this project is to
-                            develop different game-making techniques while also learning networking.
-                        </p>
-                        <img src="rsc/TotK.PNG" alt="Game main menu" />
-                    </div>
-                </li>
-
-                <li>
-                    Foreign Exchange Trading Algorithm
-                    <div class="box">
-                        <p>
-                            Coded in the MQL5 language, by catching long term trends in the Foreign Exchange through price oscillation analysis, my latest trading 
-                            algorithm was able to return 110% profits in the last year. This algorithm is still being optimized.
-                        </p>
-                        <img src="rsc/EA.PNG" alt="Expert Advisor" />
-                    </div> 
-                </li>
-
-
-                <li>
-                    Poker Game
-                    <div class="box">
-                        <p>
-                            This python based game of poker features a complete GUI and adaptive AI for the computer 
-                            players. The CPUs will handle their decisions given the strength of their cards, with leeway to bluff the player out.
-                            Included in the repository is a study documentation, outlining the game logic and playtesting results.
-                        </p>
-                        <img src="rsc/poker.PNG" alt="poker interface" />
-                    </div>
-                </li>
-            </ol>
+            <ol type="I" id="prj"></ol>
+            <?php 
+                $i = 0;
+                while($i < sizeof($lines)){
+                    $name = $lines[$i];
+                    $description = $lines[$i + 1];
+                    echo "<script type='text/javascript'>
+                        createProject(`$name`, `$description`);
+                        </script>";
+                    $i += 2;
+                }
+            ?>
         </div>
     </body>
 </html>
